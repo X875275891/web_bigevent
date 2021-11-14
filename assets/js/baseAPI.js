6708d90e886277176e60dcf435a7c18b65f19f5e
“keshui” <X875275891@163.com>
2021-11-14 21:29:47 +0800

    完成个人中心功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -15,7 +15,7 @@ $.ajaxPrefilter(function(options){
     }
    
     // 全局统一挂载complete回调函数
-    options.complete = complete: function(res){
+    options.complete =  function(res){
         // console.log('执行了complete函数')
         // console.log(res);
         // 在complete回调函数中，可以使用res.responseJSON拿到服务器相应回来的数据
